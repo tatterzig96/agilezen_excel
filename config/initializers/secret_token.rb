d7aafbd184f3467540f42e90951ca250a3c7aaa7
tatterzig96 <marktatts@gmail.com>
2011-09-24 18:36:08 -0500

    Project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgilezenExcel::Application.config.secret_token = '761da66c30635b0ec61ab1e79b69009d34e969ace6bb29b12dc7d82a62d27c03581839f95cfa051d995054b5c45b341c3d0fc07dc56ff0fd5ce9ac7809dd42cb'
